dab53f9c030fc14fc29698fc648f06fd9f6532f8
raj tayde <rajtayde0607@gmail.com>
2025-11-13 22:33:12 +0530

    feat(team): added complete team role

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -1,6 +1,5 @@
  // src/data/teamData.js
 
-// ------------------ IMPORT ALL IMAGES ------------------
 import aayush from "../assets/team/aayush.jpg";
 import akash from "../assets/team/akash.jpeg";
 import savvy from "../assets/team/savvy-image.jpg";
@@ -32,8 +31,8 @@ import akshat from "../assets/team/akshat.png";
 import akshay from "../assets/team/akshay.png";
 
 
-// ------------------ CLEAN STRUCTURED TEAM DATA ------------------
 const teamData = [
+
   {
     name: "Aayush Agrawal",
     role: "Co-organizer — GDG Cloud Indore",
@@ -54,10 +53,10 @@ const teamData = [
 
   {
     name: "Savvy Jain",
-    role: "WTM Ambassador & Co-organizer — GDG Indore",
+    role: "WTM Ambassador & GDG Indore Co-organizer",
     image: savvy,
     description:
-      "Savvy loves data and everything around it. CEO & Founder of Cofiato.",
+      " Savvy loves data and everything around it. She used to be a data scientist and has experience working in various domains. She is currently CEO and Founder of Cofiato, which is a Hardware Intelligence firm. Her second love is community evangelism. She's associated with many technical and networking communities like GDG, GCDC, WTM, Linkedin Local, etc. She is also Women in Data Science Regional Ambassador. She likes innovation, creativity, and coffee.",
     linkedin: "https://www.linkedin.com/in/savvyjain",
     twitter: "https://www.twitter.com/savvyjain",
   },
@@ -67,16 +66,18 @@ const teamData = [
     role: "Core Team Member — GDG Indore",
     image: darshan,
     description:
-      "Tech enthusiast, open source contributor & Dot Net Developer.",
+      "Tech enthusiast, Open source contributor, GDG Core Team Member & full-time Dot Net Developer at Workpulse",
     linkedin: "https://www.linkedin.com/in/xyresic-darshan",
-    twitter: "https://twitter.com/xyresic_darshan",
+    twitter:
+      "https://twitter.com/xyresic_darshan?t=zbVlbv2XtNjlMziddoasrw&s=09",
   },
 
   {
     name: "Manas Bhandari",
     role: "Core Team Member — GDG Indore",
     image: manas,
-    description: "Community enthusiast, data science & web development.",
+    description:
+      "Community enthusiasts having interest in data science and web development.",
     linkedin: "https://www.linkedin.com/in/manas0609/",
     twitter: "",
   },
@@ -86,27 +87,27 @@ const teamData = [
     role: "Core Team Member — GDG Cloud Indore & GDG Indore",
     image: sarika,
     description:
-      "Self motivated, enthusiastic learner and an excellent team leader.",
+      "A Believer, Self Motivated, Practical & Enthusiastic Folk with keen behaviour to learn new things, And A Good Team Player as well as Had Good Ability To Lead the Team..!!",
     linkedin: "https://www.linkedin.com/in/sarika-purohit",
     twitter: "https://twitter.com/SarikaPurohit5",
   },
 
   {
     name: "Tanushka Mangal",
-    role: "Core Team Member & Graphic Designer — GDG Indore",
+    role: "Core Team Member — GDG Indore",
     image: tanushka,
     description:
-      "Final year CSE student. Passionate about programming and web apps.",
+      "Final year B. Tech focused in Computer Science and Engineering student. Passionate about programming and developing web apps. Learner, explorer of technology.",
     linkedin: "https://www.linkedin.com/in/tanushka-mangal",
     twitter: "",
   },
 
   {
     name: "Priyal Jain",
-    role: "WTM Ambassador & Co-organizer — GDG Cloud Indore",
+    role: "WTM Ambassador — GDG Cloud Indore",
     image: priyal,
     description:
-      "UI developer, community lover & open source contributor.",
+      "I am UI developer and keen to learn new things, explore about new technologies and challenges. Also, love to contribute in open source communities. Enthusiast tech lover, community lover, dreamer and achiever.",
     linkedin: "https://www.linkedin.com/in/priyaljain919/",
     twitter: "https://twitter.com/priyaljain919/",
   },
@@ -115,7 +116,8 @@ const teamData = [
     name: "Adeen Shukla",
     role: "Co-organizer — GDG Indore",
     image: adeen,
-    description: "Community Organizer | Web & Mobile Developer",
+    description:
+      "Community Organizer | Web and Mobile Applications developer",
     linkedin: "https://linkedin.com/in/adeen-s",
     twitter: "https://twitter.com/AdeenShukla",
   },
@@ -124,34 +126,38 @@ const teamData = [
     name: "Nivesh Gaikwad",
     role: "Core Team Member — GDSC SDSF DAVV",
     image: nivesh,
-    description: "AI & Data Science explorer. Fast learner.",
+    description:
+      "An effective and fast learner who understands new technology, AI & Data Science explorer.",
     linkedin: "https://www.linkedin.com/in/nivesh-gaikwad-946175230",
-    twitter: "",
+    twitter:
+      "https://twitter.com/gaikwad_nivesh?t=WpPmR6fVlW9eJ1ze08PVNA&s=09",
   },
 
   {
     name: "Mradula Mittal",
     role: "Core Team Member — GDG & WTM Indore",
     image: mradula,
-    description: "Content writer | Django developer | ML enthusiast.",
+    description:
+      "A tech community enthusiast with a knack for content writing || A django developer || a machine learning enthusiast.",
     linkedin: "https://www.linkedin.com/in/mradula-mittal/",
     twitter: "https://twitter.com/MittalMradula",
   },
 
   {
     name: "Shachita Jain",
-    role: "SDE-II @ Evertz | WTM Ambassador",
+    role: "Co-organizer — GDG Cloud Indore",
     image: shachita,
-    description: "Software Developer Engineer II at Evertz.",
+    description: "SDE - II,Evertz ",
     linkedin: "https://www.linkedin.com/in/shachita/",
     twitter: "https://twitter.com/shachitajain",
   },
 
   {
     name: "Karan Mittal",
-    role: "Organizer — TensorFlow User Group Indore",
+    role: "Organizer — Tensorflow User Group Indore",
     image: karan,
-    description: "Data Scientist / Data Engineer.",
+    description:
+      "I am a Data Scientist - Data Engineer, giving solutions and insights with Data.",
     linkedin: "https://www.linkedin.com/in/karansmittal/",
     twitter: "https://twitter.com/KaranSMittal",
   },
@@ -160,7 +166,8 @@ const teamData = [
     name: "Akhil Parikh",
     role: "Core Team Member — GDG Indore",
     image: akhil,
-    description: "Associate Consultant at Wipro | Crowdsource Influencer",
+    description:
+      "Working as Associate Consultant in Wipro Ltd. Also a Google Crowdsource Influencer ",
     linkedin: "https://www.linkedin.com/in/akhilparikh",
     twitter: "https://twitter.com/Akhilparikh_",
   },
@@ -178,7 +185,7 @@ const teamData = [
     name: "Tushar Pal",
     role: "Organizer — GDG Indore",
     image: tushar,
-    description: "Co-Founder @ Protonshub Technologies",
+    description: "Co-Founder @ Protonshub Technologies ",
     linkedin: "https://www.linkedin.com/in/tushar-p-325b5869/",
     twitter: "https://twitter.com/tpal94",
   },
@@ -196,7 +203,8 @@ const teamData = [
     name: "Anurag Mishra",
     role: "Core Team Member — GDG Indore",
     image: anurag,
-    description: "Software Engineer | ML Enthusiast",
+    description:
+      "Software Engineer at PayU Payments | SIH 2022 Finalist | GDG Indore | Google DSC Lead 2021-22 | Machine Learning | MLSA ",
     linkedin: "https://www.linkedin.com/in/-anuragmishra/",
     twitter: "https://twitter.com/anurag_ayu",
   },
@@ -205,7 +213,8 @@ const teamData = [
     name: "Vinayak Gavariya",
     role: "GDSC Lead — GDSC SDSF, DAVV",
     image: vinayak,
-    description: "Exploring Data Science and Web3.",
+    description:
+      " A community enthusiast who is exploring Data science and Web3.",
     linkedin: "https://www.linkedin.com/in/vinayakgavariya/",
     twitter: "https://twitter.com/gavariyavinayak",
   },
@@ -215,7 +224,7 @@ const teamData = [
     role: "Co-Lead — GDSC IET DAVV",
     image: tanmay,
     description:
-      "Graphic designer | Python/C++ coder | ML explorer.",
+      "I have a open minded sense of growth and support, I love to volunteer and contribute to field of my interest and would like to explore more! I have ample of experience in Graphic Designing, Coding with Python and C++. I share interests in flutter, ML, Problem solving and many yet to be known.",
     linkedin: "https://www.linkedin.com/in/tanmaybhadviya/",
     twitter: "",
   },
@@ -224,16 +233,18 @@ const teamData = [
     name: "Rishita Gupta",
     role: "Social Media Lead — GDSC SDSF DAVV",
     image: rishita,
-    description: "SQL | Python | C++ | Graphic Design",
+    description:
+      "Social Media Lead @GDSCSDSF DAVV |SQL|C++|Python| Graphic Design",
     linkedin: "https://www.linkedin.com/in/rishita-gupta-41575b24a/",
     twitter: "",
   },
 
   {
     name: "Rohit Chaudhary",
-    role: "GOLD MLSA | Application Engineer",
+    role: "GOLD MLSA",
     image: rohit,
-    description: "Tech community enthusiast.",
+    description:
+      "GOLD MLSA || Application Engineer || Tech Community Enthusiastic",
     linkedin: "https://www.linkedin.com/in/rohit-chaudhary-7697781a7/",
     twitter: "",
   },
@@ -243,26 +254,27 @@ const teamData = [
     role: "Core Team Member — GDG Indore",
     image: atharav,
     description:
-      "Frontend developer | Mentor | Postman Student Expert",
+      "Passionate undergraduate computer science student with a strong programming foundation, seeking exciting roles in product and service-based companies. Committed to applying technical skills, problem-solving, and collaborating with teams to develop innovative solutions while staying updated with evolving technologies.",
     linkedin: "https://www.linkedin.com/in/atharv-vani110/",
     twitter: "https://twitter.com/atharv_110",
   },
 
   {
     name: "Asmita Bhadoria",
-    role: "Google DSC Lead '22 — NMIMS Indore",
+    role: "Google DSC Lead'22 — NMIMS, Indore",
     image: asmita,
-    description: "Android & Jetpack Compose developer.",
-    linkedin:
-      "https://www.linkedin.com/in/asmita-bhadoria-59025622a/",
+    description:
+      "Google DSC Lead'22 | NMIMS, Indore | Jetpack Compose Kotlin and Android Studio user and tutor",
+    linkedin: "https://www.linkedin.com/in/asmita-bhadoria-59025622a/",
     twitter: "",
   },
 
   {
     name: "Drishti Lalwani",
-    role: "Google DSC Lead '23 — Acropolis",
+    role: "Google DSC Lead'23 — Acropolis",
     image: drishti,
-    description: "Third year CSE student & proactive learner.",
+    description:
+      "I am a third year computer science engineering student and a proactive learner, trying to develop skills in various domains.",
     linkedin: "https://www.linkedin.com/in/drishti-lalwani/",
     twitter: "",
   },
@@ -280,30 +292,33 @@ const teamData = [
     name: "Mustafa Azad",
     role: "Full-Stack Developer — GDG Indore",
     image: mustafa,
-    description: "Full-Stack Developer | Open Source Contributor",
+    description:
+      "Full-Stack Web Developer | Open Source Contributor",
     linkedin: "https://www.linkedin.com/in/mustafaazad03/",
     twitter: "https://twitter.com/mustafaazad03",
   },
 
   {
     name: "Akshat Patel",
-    role: "Core Team Member — GDG Indore | GDSC Lead '21",
+    role: "Core Team Member — GDG Indore",
     image: akshat,
     description:
-      "Developer | Ex GDSC Lead | Traveler | Community mentor",
+      "I am Akshat Patel, Former GDSC Lead and Core Member of GDG Indore. I love to connect with new people and explore new things. I also love to travel to different places. As a developer, I am making some creative things and I love to share my knowledge with the community.",
     linkedin: "https://www.linkedin.com/in/akshat5129",
     twitter: "https://twitter.com/akshat5129",
   },
 
   {
     name: "Akshay Malviya",
-    role: "Senior Software Engineer — Capgemini",
+    role:
+      "Senior Software Engineer at Volvo Group IT via Capgemini Engineering Nordics",
     image: akshay,
     description:
-      "Software Engineer | Former GDG Cloud Organizer",
+      "Software Engineer with a demonstrated history of working in the information technology and services industry. Former Community Organizer for GDG Cloud Indore, an independent developer community from Google Developers. Skilled in Data Science, Python, Databases, and ERP. Strong engineering professional with a Bachelor’s Degree focused in Computer Science Engineering.",
     linkedin: "https://www.linkedin.com/in/akshaymalviya/",
     twitter: "https://twitter.com/imakshaymalviya",
   },
+
 ];
 
 export default teamData;
