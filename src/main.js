bba0cc6aad4a55d645bff5f186b66b090c53d437
Shivam Joshi <110561816+Shivam182004@users.noreply.github.com>
2025-12-06 12:55:15 +0530

    Revert "feat : added firebase for analytical treaking "

--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import 'vuetify/styles';
-
-// Firebase and Analytics
-import './firebase.js'; 
-import { analytics } from '@/firebase.js';
-import { logEvent } from 'firebase/analytics'; 
+import 'vuetify/styles'; 
 
 // Material Design Icons
 import '@mdi/font/css/materialdesignicons.css'
@@ -17,24 +12,11 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-// Create Vuetify instance
+// reate Vuetify instance
 const vuetify = createVuetify({
   components,
   directives,
 })
-
-
-
-// to log custom event on Firebase Analytics
-router.afterEach((to) => {
-  if (analytics) {
-    logEvent(analytics, 'page_view', {
-      page_title : to.name,
-      page_path: to.fullPath
-    })
-  }
-})
-
 
 // Mount the app
 createApp(App)
